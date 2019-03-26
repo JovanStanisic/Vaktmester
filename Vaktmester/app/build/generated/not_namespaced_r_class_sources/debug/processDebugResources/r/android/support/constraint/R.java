132137028f69f67a419420bb726a8f4ccaaacb2c
android <stanaboy95@gmail.com>
2019-03-26 09:08:16 +0100

    Cas 26/03/19

--- Vaktmester/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ Vaktmester/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f080022;
-        public static final int end = 0x7f080039;
-        public static final int gone = 0x7f080042;
-        public static final int invisible = 0x7f08004d;
-        public static final int left = 0x7f080050;
-        public static final int packed = 0x7f08005f;
-        public static final int parent = 0x7f080060;
-        public static final int percent = 0x7f080062;
-        public static final int right = 0x7f080066;
-        public static final int spread = 0x7f08007e;
-        public static final int spread_inside = 0x7f08007f;
-        public static final int start = 0x7f080084;
-        public static final int top = 0x7f080096;
-        public static final int wrap = 0x7f08009c;
+        public static final int bottom = 0x7f080021;
+        public static final int end = 0x7f080037;
+        public static final int gone = 0x7f08003f;
+        public static final int invisible = 0x7f08004a;
+        public static final int left = 0x7f08004d;
+        public static final int packed = 0x7f08005b;
+        public static final int parent = 0x7f08005c;
+        public static final int percent = 0x7f08005e;
+        public static final int right = 0x7f080062;
+        public static final int spread = 0x7f08007a;
+        public static final int spread_inside = 0x7f08007b;
+        public static final int start = 0x7f080080;
+        public static final int top = 0x7f080091;
+        public static final int wrap = 0x7f080097;
     }
     public static final class styleable {
         private styleable() {}
